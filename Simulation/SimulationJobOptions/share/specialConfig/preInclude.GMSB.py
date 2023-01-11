69347b6eda20cc1658fc4c29237aaac7b2dd6c2a
Ewelina Maria Lobodzinska <ewelina.maria.lobodzinska@cern.ch>
2023-01-11 14:54:05 +0100

    Update preInclude.GMSB.py

--- Simulation/SimulationJobOptions/share/specialConfig/preInclude.GMSB.py
+++ Simulation/SimulationJobOptions/share/specialConfig/preInclude.GMSB.py
@@ -1,20 +1,18 @@
-#################################################################
-#       preInclude.GMSB.py - Sascha Mehlhase, 6 Jul 2016                #
-#################################################################
+##############################################################################################################
+#       preInclude.GMSB.py - Sascha Mehlhase, 6 Jul 2016 --> edited May 2020 by R. Kopeliansky               #
+##############################################################################################################
 
 def get_and_fix_PDGTABLE(replace):
     import os, shutil, re
 
     # Download generic PDGTABLE (overwrite existing one if it exists)
-    from G4AtlasApps.SimFlags import simFlags
-    from ExtraParticles.PDGHelpers import getLocalPDGTableName
-    tableName = getLocalPDGTableName(simFlags.ExtraParticlesPDGTABLE.get_Value())
-    shutil.move( tableName,  tableName+'.backup')
+    os.system('get_files -remove -data PDGTABLE.MeV')
+    shutil.move('PDGTABLE.MeV', 'PDGTABLE.MeV.org')
 
     # an example line to illustrate the fixed format, see PDGTABLE.MeV for more details
     # M 1000022                          0.E+00         +0.0E+00 -0.0E+00 ~chi(0,1)     0
 
-    lines = open(tableName+'.backup','r').readlines()
+    lines = open('PDGTABLE.MeV.org').readlines()
     for pdgid,mass,name,charge in replace:
         if not re.search(r'[MW]\s+'+str(pdgid)+'\s+\S+', ''.join(lines)):
             lines.append('M' + str(pdgid).rjust(8) +''.ljust(26) +
@@ -37,28 +35,26 @@ def get_and_fix_PDGTABLE(replace):
     print 'modfied PDGTABLE\n%s\n' % ''.join(lines)
     sys.stdout.flush()
 
-
 def load_files_for_GMSB_scenario(simdict):
 
     GMSBIndex = int(simdict["GMSBIndex"])
-    pdgcodes = []
+
     if GMSBIndex == 1:
         # get_and_fix_PDGTABLE([('M', '1000022', eval(simdict["GMSBNeutralino"])), ('M', '1000039', eval(simdict.get("GMSBGravitino",'0')))])
         get_and_fix_PDGTABLE([
-                              (1000022, eval(simdict["GMSBNeutralino"]), '~chi(0,1)', '0'),
+                             # (1000022, eval(simdict["GMSBNeutralino"]), '~chi(0,1)', '0'),
                               (1000039, eval(simdict.get("GMSBGravitino",'0')), '~G', '0')
                             ])
-        pdgcodes += [1000022,1000039]
+
     elif GMSBIndex == 2:
         m_stau    = eval(simdict["GMSBStau"])
-        m_slepton = eval(simdict["GMSBSlepton"])
+        m_neutralino = eval(simdict["GMSBneutralino"])
         # get_and_fix_PDGTABLE([('M', '1000015', m_stau), ('M', '2000011', m_slepton), ('M', '2000013', m_slepton)])
         get_and_fix_PDGTABLE([
                               (1000015, m_stau, '~tau(L)', '-'),
-                              (2000011, m_slepton, '~e(R)', '-'),
-                              (2000013, m_slepton, '~mu(R)', '-')
+                              (1000022, m_neutralino, '~chi(0,1)', '-'),
                             ])
-        pdgcodes += [-1000015,1000015,-2000011,2000011,-2000013,2000013]
+
     elif GMSBIndex == 3:
         m_stau = eval(simdict["GMSBStau"])
         m_slepton = eval(simdict["GMSBSlepton"])
@@ -96,32 +92,16 @@ def load_files_for_GMSB_scenario(simdict):
                               (1000035, 1.00E+04, '~chi(0,4)', '0'),
                               (1000037, 1.00E+04, '~chi(+,2)', '+')
                             ])
-        pdgcodes += [
-            -1000001,1000001,-2000001,2000001,
-            -1000002,1000002,-2000002,2000002,
-            -1000003,1000003,-2000003,2000003,
-            -1000004,1000004,-2000004,2000004,
-            -1000005,1000005,-2000005,2000005,
-            -1000006,1000006,-2000006,2000006,
-            -1000011,1000011,-2000011,2000011,
-            -1000013,1000013,-2000013,2000013,
-            -1000015,1000015,-2000015,2000015,
-             1000012,1000014,1000016,1000021,1000022,1000023,
-            -1000024,1000024,1000025,1000035,-1000037,1000037]
 
     elif GMSBIndex == 4:
         # get_and_fix_PDGTABLE([('M', '1000015', eval(simdict["GMSBStau"]))])
         get_and_fix_PDGTABLE([
                               (1000015, m_stau, '~tau(L)', '-')
                             ])
-        pdgcodes += [-1000015,1000015]
 
     else:
         print 'GMSBIndex %i not supported' % GMSBIndex
         raise
-    from ExtraParticles.PDGHelpers import updateExtraParticleWhiteList
-    updateExtraParticleWhiteList('G4particle_whitelist_ExtraParticles.txt', pdgcodes)
-
 
 doG4SimConfig = True
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
@@ -132,9 +112,6 @@ try:
     if "StreamHITS" in f.infos["stream_names"]:
         from Digitization.DigitizationFlags import digitizationFlags
         simdict = digitizationFlags.specialConfiguration.get_Value()
-        if simdict is None:
-            # Here we are in a ReSim job, so the input is a HITS file
-            raise ValueError
         doG4SimConfig = False
     else:
         from G4AtlasApps.SimFlags import simFlags
@@ -151,12 +128,13 @@ if doG4SimConfig:
     GMSBIndex = eval(simdict["GMSBIndex"])
     from G4AtlasApps.SimFlags import simFlags
     if GMSBIndex == 1: # generic neutralino to photon scenario
-        simFlags.PhysicsOptions += ["GauginosPhysicsTool","NeutralinoToPhotonGravitino"]
+        simFlags.PhysicsOptions += ["GauginosPhysicsTool"]
+        simFlags.PhysicsOptions += ["NeutralinoToPhotonGravitino"]
     elif GMSBIndex == 2 or GMSBIndex == 3 or GMSBIndex == 4: # generic stau scenario
         simFlags.PhysicsOptions += ["SleptonsPhysicsTool"]
-    else:
-        print ('GMSBIndex %i not supported' % GMSBIndex)
-        raise
     del GMSBIndex
 
 del doG4SimConfig, simdict
+
+
+
