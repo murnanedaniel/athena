222223be8de8b78518e79a1dc6a2c50797908bb7
John Derek Chapman <chapman@hep.phy.cam.ac.uk>
2023-02-24 17:12:21 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- Simulation/ISF/ISF_Acts/ISF_ActsTools/python/ISF_ActsToolsConfig.py
+++ Simulation/ISF/ISF_Acts/ISF_ActsTools/python/ISF_ActsToolsConfig.py
@@ -8,6 +8,9 @@ from AthenaConfiguration.ComponentFactory import CompFactory
 def ActsFatrasSimToolCfg(flags, name="ISF_ActsFatrasSimTool", **kwargs):
     """Return ISF_FatrasSimHitCreatorID configured with ComponentAccumulator"""
     acc = ComponentAccumulator()
+    from ActsGeometry.ActsGeometryConfig import ActsTrackingGeometryToolCfg
+    kwargs.setdefault('TrackingGeometryTool', acc.popToolsAndMerge(ActsTrackingGeometryToolCfg(flags)))
+
     kwargs.setdefault("MaxSteps", 1500)
     acc.setPrivateTools(CompFactory.ISF.ActsFatrasSimTool(name, **kwargs))
     return acc
