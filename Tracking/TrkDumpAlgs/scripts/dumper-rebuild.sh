36eaafc59d80356f9d66d781cabbf6455357857b
Christophe Collard <christophe.collard@l2it.in2p3.fr>
2023-02-27 18:02:48 +0100

    update 20230227-4

--- Tracking/TrkDumpAlgs/scripts/dumper-rebuild.sh
+++ Tracking/TrkDumpAlgs/scripts/dumper-rebuild.sh
@@ -3,7 +3,8 @@ alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup git
 asetup 21.9,Athena,21.9.26
-mkdir build
+#mkdir build
+cd athena
 source build/x86_64-centos7-gcc62-opt/setup.sh
 cd build
 cmake -DATLAS_PACKAGE_FILTER_FILE=../package_filters.txt ../athena/Projects/WorkDir
