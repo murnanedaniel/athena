cc6384b3bae8878dc4fb3c8f48cc62008830c6ff
Jan-Eric Nitschke <jan-eric.nitschke@cern.ch>
2023-05-05 13:17:34 +0200

    Updated year in copyright notice

--- DataQuality/ZLumiScripts/scripts/Pandas_scripts/tools/zlumi_alleff.py
+++ DataQuality/ZLumiScripts/scripts/Pandas_scripts/tools/zlumi_alleff.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration                   
+# Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration                   
 
 import sys, os, glob
 import ROOT
